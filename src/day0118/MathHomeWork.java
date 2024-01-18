8c250c3b76bb7a0cf18d6209b8c21d2480bc3329
choiyeon <2dpaesu@gmail.com>
2024-01-19 00:31:09 +0900

    homework commit

--- src/day0118/MathHomeWork.java
+++ src/day0118/MathHomeWork.java
@@ -1,7 +1,6 @@
 package day0118;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * 1. 아래의 데이터로 배열을 만들고, 임의의 방하나의 값을 출력<br>
@@ -12,26 +11,33 @@ import java.util.Scanner;
  */
 public class MathHomeWork {
 
+	/**
+	 * "어쩔, 저쩔, 뇌절, 안물, 안궁, 때리, 못때리, 개킹" 중에 하나 출력
+	 */
 	public MathHomeWork() {
 		String[] noJam = { "어쩔", "저쩔", "뇌절", "안물", "안궁", "때리", "못때리", "개킹" };
-		double num = Math.random();
-		int i = (int) (num * 8);
+		int i = (int) (Math.random() * 8);
 		System.out.println(noJam[i]);
 	}// MathHomeWork
 
+	/**
+	 * 8자리의 영문자, 숫자로 구성된 임의의 비밀번호 반환
+	 * 
+	 * @return 8자리 비밀번호
+	 */
 	public String printPassWord() {
 		// 대문자(65-90)26자 소문자(97-122)26자 숫자(48-57)10자
 		char[] secret = new char[8];
-		for (int j = 0; j < secret.length; j++) {
+		for (int i = 0; i < secret.length; i++) {
 			switch ((int) (Math.random() * 3)) {
 			case 0:
-				secret[j] = (char) ((Math.random() * 26) + 65);// 대문자영어
+				secret[i] = (char) ((Math.random() * 26) + 65);// 대문자영어
 				break;
 			case 1:
-				secret[j] = (char) ((Math.random() * 26) + 97);// 소문자영어
+				secret[i] = (char) ((Math.random() * 26) + 97);// 소문자영어
 				break;
 			case 2:
-				secret[j] = (char) ((Math.random() * 10) + 48);// 숫자
+				secret[i] = (char) ((Math.random() * 10) + 48);// 숫자
 				break;
 			}// switch
 
@@ -48,17 +54,43 @@ public class MathHomeWork {
 	 */
 	public String printLotto() {
 		int[] lottoList = new int[6];
+		int next = 0;
+
+		do {
+			int num = (int) (Math.random() * 45) + 1;
+			boolean duplication = false;
+
+			// 중복 체크
+			for (int j = 0; j < lottoList.length; j++) {
+				if (lottoList[j] == num) {
+					duplication = true;
+					break;
+				} // if
+			} // for
+
+			// 중복이 없으면 추가
+			if (!duplication) {
+				lottoList[next] = num;
+				next++;
+			} // if
+		} while (next < lottoList.length);
+
+		// 배열 정렬
+		int temp = 0;
 		for (int i = 0; i < lottoList.length; i++) {
-			for (int j = 1; j < lottoList.length; j++) {
-				if (lottoList[i] != lottoList[j]) {
-					lottoList[j] = (int) (Math.random() * 44);// 숫자(1~45)
+			for (int j = i + 1; j < lottoList.length; j++) {
+				if (lottoList[i] > lottoList[j]) {
+					temp = lottoList[i];
+					lottoList[i] = lottoList[j];
+					lottoList[j] = temp;
 				} // if
 			} // for j
 		} // for i
-		Arrays.sort(lottoList);
+
+		// 배열 출력
 		String lotto = Arrays.toString(lottoList);
 		return lotto;
-	}
+	}// printLotto
 
 	public static void main(String[] args) {
 		MathHomeWork mhw = new MathHomeWork();
