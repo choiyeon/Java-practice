bde8c8c76c1798fb4a787d967960bc19598790d1
choiyeon <2dpaesu@gmail.com>
2024-01-23 22:47:57 +0900

    hw commit

--- src/day0123/HomeWork0123.java
+++ src/day0123/HomeWork0123.java
@@ -15,95 +15,84 @@ import javax.swing.JOptionPane;
  * 2 김병년 95 89 184<br>
  * 
  * csv형태가 아닌 Y나 y입력되면 종료
+ * 
+ * 값을 쪼개서 변수에 넣고, list.add로 get값 넣고, list크기만큼 출력. list.get. y나Y면 break
  */
 public class HomeWork0123 {
-	//입력받기
-	String inputData = JOptionPane.showInputDialog("\"이름,자바점수,오라클점수\" 입력\n\"출력\"을 입력하면 출력됩니다.\n 종료하시면 \"Y / y\"을 입력해주세요");
-	
+	String informData;
+	static Boolean LOOP = true;
+	List<String> nameList = new ArrayList<String>();
+	List<Integer> javaScoreList = new ArrayList<Integer>();
+	List<Integer> oracleScoreList = new ArrayList<Integer>();
+
 	public HomeWork0123() {
-		
-	}//HomeWork0123
-	
-	
-	/**
-	 * 메뉴 입력받기
-	 */
+	}// HomeWork0123
+
 	public void inputMenu() {
-		
-	}//inputMenu
-	
-	
+		informData = JOptionPane.showInputDialog("\"이름,자바점수,오라클점수\" 입력\n\"출력\"을 입력하면 출력됩니다.\n 종료하시면 \"Y / y\"을 입력해주세요");
+	}// inputMenu
+
 	/**
 	 * 입력받은 값 쪼개기
+	 * 
 	 * @return name, javaScore, oracleScore
 	 */
-	@SuppressWarnings("unchecked")
 	public WorkVO workData() {
 		WorkVO wkVO = null;
-		StringTokenizer stk = new StringTokenizer(inputData, ", ");
-		
-		int tokenCnt = stk.countTokens();
-		List dataList = new ArrayList();
-		
-		while(stk.hasMoreTokens()) {
-			String data = stk.nextToken();
-			dataList.add(data);
-		}//end while
-		
-		String name = (String) dataList.get(0);
-		int javaScore = (int) dataList.get(1);
-		int oracleScore = (int) dataList.get(2);
-		
+		StringTokenizer stk = new StringTokenizer(informData, ",");
+
+		String name = stk.nextToken().trim();
+		int javaScore = Integer.parseInt(stk.nextToken().trim());
+		int oracleScore = Integer.parseInt(stk.nextToken().trim());
+
 		wkVO = new WorkVO(name, javaScore, oracleScore);
-		
+
 		return wkVO;
-	}//workData
-	
+	}// workData
+
 	/**
-	 * 데이터 입력받아서 리스트에 넣기
+	 * 데이터 입력받아서 리스트에 넣기..?
+	 * 
 	 */
 	public void inputData() {
 		WorkVO wkVO = workData();
-		wkVO.getName();
-		wkVO.getJavaScore();
-		wkVO.getOracleScore();
-		
-	}//inputData
-	
+		nameList.add(wkVO.getName());
+		javaScoreList.add(wkVO.getJavaScore());
+		oracleScoreList.add(wkVO.getOracleScore());
+	}// inputData
+
 	/**
 	 * 데이터 출력
 	 */
 	public void printData() {
-		WorkVO wkVO = workData();
-		int i = 1;
-		if(("출력").equals(inputData)) {
-			System.out.println("번호\t이름\t자바\t오라클\t총점\n");
-			System.out.println(i + "\t" + wkVO.getName()+ "\t" + wkVO.getJavaScore() + "\t" + 
-			wkVO.getOracleScore() + "\t" + "총점");
-		}
-		
-	}//printData
-	
+		System.out.println("번호\t이름\t자바\t오라클\t총점");
+		System.out.println("========================================");
+		for (int i = 0; i < nameList.size(); i++) {
+			System.out.println((i + 1) + "\t" + nameList.get(i) + "\t" + javaScoreList.get(i) + "\t"
+					+ oracleScoreList.get(i) + "\t" + (javaScoreList.get(i) + oracleScoreList.get(i)));
+		} // end for
+	}// printData
+
 	/**
 	 * 프로그램 종료
 	 */
 	public void exitProgram() {
-		if(("Y").equals(inputData) || ("y").equals(inputData)) {
-			System.out.println("입력을 종료합니다.");
-		}
-	}//exitProgram
-	
+		System.out.println("입력을 종료합니다.");
+		LOOP = false;
+	}// exitProgram
+
 	public static void main(String[] args) {
 		HomeWork0123 hw = new HomeWork0123();
-//		hw.workData();
-
-//		String inputData = JOptionPane.showInputDialog("이름을 입력해주세요","홍길동");
-//		StringBuilder sb = new StringBuilder();
-//		if("윤웅찬".equals(inputData)) {
-//			sb.append("조원동의 자랑 ");
-//		}
-//		sb.append(inputData).append("님");
-//		System.out.println(sb);
+		while (LOOP) {
+			hw.inputMenu();
+			if (("출력").equals(hw.informData)) {
+				hw.printData();
+			} else if (("Y").equals(hw.informData) || ("y").equals(hw.informData)) {
+				hw.exitProgram();
+			} else if (hw.informData != null) {
+				hw.inputData();// 값 넣기 호출
+			}
+		} // while
 
 	}// main
 
